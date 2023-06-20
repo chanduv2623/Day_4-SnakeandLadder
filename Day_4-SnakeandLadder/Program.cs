076e5513de1eb7e89d1d4889874bd8cb062600b1
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-06-20 20:22:33 +0530

    Player rolled a dice to get 1-6

--- Day_4-SnakeandLadder/Program.cs
+++ Day_4-SnakeandLadder/Program.cs
@@ -6,7 +6,12 @@
         {
             Console.WriteLine("welcome to the snake and ladder program!");
             int PlayerPosition = 0;
-            Console.WriteLine("singleplayer position is now starting at zero");
+            Console.WriteLine("singleplayer position is now starting as zero");
+            int playerNumberOfRolls = 0;
+            Random random = new Random();
+            int playerDiceRoll = random.Next(1, 7);
+            playerNumberOfRolls++;
+            Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
         }
     }
 }
