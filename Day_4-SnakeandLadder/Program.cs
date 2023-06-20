02e4a179cd10ac30a8371537bae4f9206c3a7c8f
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-06-20 20:03:12 +0530

    Welcome to Snake & Ladder

---
+++ Day_4-SnakeandLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day_4_SnakeandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the snake and ladder program!");
+            int PlayerPosition = 0;
+            Console.WriteLine("singleplayer position is now starting at zero");
+        }
+    }
+}
